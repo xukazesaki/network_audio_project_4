d2617f5d05adf3ed79d66e22dbdeb62dca6a5c97
xukazesaki <xukazesaki@gmail.com>
2026-03-20 10:18:56 +0800

    26.3.20 10：18

---
+++ start_server.ps1
@@ -0,0 +1,8 @@
+$projectRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
+$srcDir = Join-Path $projectRoot "src"
+
+Start-Process powershell -Verb RunAs -ArgumentList @(
+    "-NoExit",
+    "-Command",
+    "Set-Location '$srcDir'; py server.py"
+)
