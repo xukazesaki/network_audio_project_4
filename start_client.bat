42b84c31555d81be38ba4056f6229720765bd6ff
XuRui <213232039@seu.edu.cn>
2026-03-27 14:01:12 +0800

    稍微加了个清除缓存的逻辑

--- start_client.bat
+++ start_client.bat
@@ -1,4 +1,5 @@
 @echo off
 cd /d "%~dp0"
 python -m src.apps.client_stream_gui
+for /d /r %%D in (__pycache__) do @if exist "%%D" rd /s /q "%%D"
 pause
--- start_server.bat
+++ start_server.bat
@@ -1,4 +1,5 @@
 @echo off
 cd /d "%~dp0"
 python -m src.server.server
+for /d /r %%D in (__pycache__) do @if exist "%%D" rd /s /q "%%D"
 pause
